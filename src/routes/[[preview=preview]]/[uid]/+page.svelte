08b12ff634980faec31e95be020e20441ab1509f
Marius Jopen <mail@mariusjopen.com>
2024-09-04 00:02:07 +0200

    components

--- src/routes/[[preview=preview]]/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/[uid]/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	import { SliceZone } from '@prismicio/svelte';
-
 	import { components } from '$lib/slices';
+	import Header from '$lib/components/header.svelte';
+	import Footer from '$lib/components/footer.svelte';
 
 	export let data;
 </script>
 
+<Header />
 <SliceZone slices={data.page.data.slices} {components} />
+<Footer />
--- src/routes/[[preview=preview]]/work/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/work/[uid]/+page.svelte
@@ -3,10 +3,14 @@
 	import { components } from '$lib/slices';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import RelatedProjects from '$lib/components/related-projects.svelte';
+	import NextProject from '$lib/components/next-project.svelte';
 
 	export let data;
 </script>
 
 <Header />
 <SliceZone slices={data.page.data.slices} {components} />
+<RelatedProjects />
+<NextProject />
 <Footer />
