0662c7a6a3678c4d4b7871a1f807017027f69a33
Marius Jopen <91463168+marius-jopen@users.noreply.github.com>
2024-09-08 12:33:54 +0200

    list

--- src/lib/slices/List/index.svelte
+++ src/lib/slices/List/index.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
+	import { PrismicRichText } from "@prismicio/svelte";
 
 	export let slice: Content.ListSlice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+<section class="box">
+	
+	<div class="border-t border-d_black">
+		{#each slice.primary.items as item}
+			<div class="flex w-full border-b border-d_black pb-4 pt-4">
+				<div class="w-1/2">
+					<p>
+						{item.headline}
+					</p>
+				</div>
+	
+				<div class="w-1/2">
+					<PrismicRichText field={item.text} />
+				</div>
+			</div>
+		{/each}
+	</div>
+	
 </section>
