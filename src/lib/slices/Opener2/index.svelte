ac44772fa9874528307af3d971512803f0f9947f
Marius Jopen <91463168+marius-jopen@users.noreply.github.com>
2024-09-08 12:17:30 +0200

    opener 2

--- src/lib/slices/Opener2/index.svelte
+++ src/lib/slices/Opener2/index.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
+	import { PrismicImage } from "@prismicio/svelte";
 
 	export let slice: Content.Opener2Slice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+<section class="box">
+	<PrismicImage class="w-full h-auto rounded-3xl" field={slice.primary.image} />
+
+	<div class="w-full rounded-3xl bg-d_lightGreen text-d_black text-center py-8 px-8 -mt-10 z-10 relative">
+		<h1>
+			{slice.primary.headline}
+		</h1>
+	</div>
+
 </section>
