9dc558e8c31ed262927ddce8405abf20a8ad0466
Marius Jopen <mail@mariusjopen.com>
2024-09-30 10:17:40 +0200

    logos

--- src/lib/slices/Logos/index.svelte
+++ src/lib/slices/Logos/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
     import { getDistanceTop, getDistanceBottom } from '../../components/distances';
+	import { PrismicImage } from "@prismicio/svelte";
 
 	export let slice: Content.LogosSlice;
 
@@ -9,5 +10,17 @@
 </script>
 
 <section class="box {distanceTop} {distanceBottom}">
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+	<div class="flex flex-wrap rounded-3xl overflow-hidden">
+		{#each slice.primary.items as item, index}
+			<div class="w-1/5 aspect-square relative">
+				<div class="hover:opacity-0 transition-opacity duration-200 absolute z-20 w-full h-full aspect-square top-0 left-0 {index % 2 === 0 ? 'bg-d_lightGreen' : 'bg-d_black'}">
+					<PrismicImage class="w-full h-full p-10" field={item.logo} />
+				</div>
+				
+				<div class="z-10 aspect-square">
+					<PrismicImage class="object-cover h-full w-full" field={item.image} />
+				</div>
+			</div>
+		{/each}
+	</div>
 </section>
