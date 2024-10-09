61ef289fbd28222b4745b81bae8eece44be6f674
Marius Jopen <mail@mariusjopen.com>
2024-10-09 20:11:59 +0200

    non clickyble

--- src/routes/[[preview=preview]]/work/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/work/[uid]/+page.svelte
@@ -19,20 +19,22 @@
 
 	workStore.set(work);
 
-	// console.log(data)
+	console.log(data)
 </script>
 
-<div class="{styleClass}">
-	<div class="hidden md:block ">
-		<Header style={styleTextClass} items={data.navigation_top.data.navigation} />
-	</div>
+{#if data.page.data.clickable}
+	<div class="{styleClass}">
+		<div class="hidden md:block ">
+			<Header style={styleTextClass} items={data.navigation_top.data.navigation} />
+		</div>
 
-	<div class="block md:hidden ">
-		<MobileHeader style={styleTextClass} items={data.navigation_top.data.navigation} />
-	</div>
+		<div class="block md:hidden ">
+			<MobileHeader style={styleTextClass} items={data.navigation_top.data.navigation} />
+		</div>
 
-	<SliceZone slices={data.page.data.slices} {components} />
-	<RelatedProjects data={data} />
-	<NextProject data={data} />
-	<Footer style={styleTextClass} data={data.navigation_bottom} />
-</div>
+		<SliceZone slices={data.page.data.slices} {components} />
+		<RelatedProjects data={data} />
+		<NextProject data={data} />
+		<Footer style={styleTextClass} data={data.navigation_bottom} />
+	</div>
+{/if}
