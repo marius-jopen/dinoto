d0b71945147ff69547a1bfa1216bdd8003ef4220
Marius Jopen <mail@mariusjopen.com>
2024-12-04 16:22:59 +0100

    header fade out

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte';
+    import { fade } from 'svelte/transition';
     import HeaderLogo from '$lib/components/header-logo.svelte';
     import Navigation from '$lib/components/navigation.svelte';
 
@@ -9,9 +10,25 @@
     export let logo = true;
 
     let distanceTop = "h-[65px]";
+    let hideHeader = false;
+
+    onMount(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY + window.innerHeight;
+            const bottomPosition = document.documentElement.scrollHeight;
+            hideHeader = scrollPosition >= bottomPosition;
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
 </script>
 
-<div class="hidden md:block {!sticky && 'fixed top-0 left-0 w-full z-50'} {distanceTop}">
+{#if !hideHeader}
+<div transition:fade={{duration: 200}} class="hidden md:block {!sticky && 'fixed top-0 left-0 w-full z-50'} {distanceTop}">
     <div class="box {!sticky && 'relative'}">
         <div class="pt-4 pb-4 flex justify-between">
             {#if !sticky}
@@ -29,6 +46,7 @@
         </div>
     </div>
 </div>
+{/if}
 
 {#if !sticky}
     <div class="hidden md:block {distanceTop}" />
