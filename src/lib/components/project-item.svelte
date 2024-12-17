41802a20674b9fe2e3511cafd541bf6bae657f0d
Marius Jopen <mail@mariusjopen.com>
2024-12-17 15:57:55 +0100

    items

--- src/lib/components/project-item.svelte
+++ src/lib/components/project-item.svelte
@@ -39,7 +39,7 @@
     </div>
 
     {#if !cloud}
-        <div class="bg-white rounded-b-2xl md:rounded-b-3xl pb-4 pt-[45px] px-4 md:px-8 -mt-8 p2 text-center">
+        <div class="bg-white rounded-b-2xl md:rounded-b-3xl pb-4 pt-[45px] px-4 md:px-8 -mt-8 p2 text-center flex flex-col justify-center items-center h-[110px]">
             {data.data.client[0]?.text}
             
             <span class="text-d_darkGray">
