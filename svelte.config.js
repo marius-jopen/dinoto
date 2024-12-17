b42f5957ba717909ae24e8bcb49ec04ae66de50e
Marius Jopen <mail@mariusjopen.com>
2024-12-17 17:12:21 +0100

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // Ignore 404s during prerendering
+        if (message.includes('404')) {
+          return;
+        }
+        // Throw other errors
+        throw new Error(message);
+      }
+    }
   },
   preprocess: vitePreprocess()
 };
