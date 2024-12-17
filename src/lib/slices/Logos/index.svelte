c689f134c4c94a085b6ebce63277dc480bca2c08
Marius Jopen <mail@mariusjopen.com>
2024-12-17 15:53:25 +0100

    logos fixed

--- src/lib/slices/Logos/index.svelte
+++ src/lib/slices/Logos/index.svelte
@@ -9,15 +9,29 @@
     let distanceBottom = getDistanceBottom(slice.primary.distance_bottom);
 </script>
 
-<section data-aos="fade-up" class=" {slice.primary.narrow ? 'box-narrow' : 'box'} {distanceTop} {distanceBottom}">
+<section data-aos="fade-up" class=" {slice.primary.narrow ? 'box-narrow' : 'box'} {distanceTop} {distanceBottom} hidden md:block">
 	<div class="flex flex-wrap rounded-3xl overflow-hidden bg-d_darkGreen">
 		{#each slice.primary.items as item, index}
 			<div class="w-1/3 md:w-1/6 aspect-square relative">
 				<div class="hover:opacity-0 transition-opacity duration-200 absolute z-20 w-full h-full aspect-square top-0 left-0 
-					{index % 2 === 0 ? 'bg-d_lightGreen' : 'bg-d_darkGreen'} 
-					md:{Math.floor(index / 6) % 2 === 0 ? 
-						(index % 2 === 0 ? 'bg-d_darkGreen' : 'bg-d_lightGreen') : 
-						(index % 2 === 0 ? 'bg-d_lightGreen' : 'bg-d_darkGreen')}">
+					{(Math.floor(index / 6) % 2 === 0) === (index % 2 === 0) ? 'bg-d_lightGreen' : 'bg-d_darkGreen'}">
+					<PrismicImage class="w-full h-full p-4 md:p-8 lg:p-10 object-contain" field={item.logo} />
+				</div>
+				
+				<div class="z-10 aspect-square">
+					<PrismicImage class="object-cover h-full w-full" field={item.image} />
+				</div>
+			</div>
+		{/each}
+	</div>
+</section>
+
+<section data-aos="fade-up" class=" {slice.primary.narrow ? 'box-narrow' : 'box'} {distanceTop} {distanceBottom} block md:hidden">
+	<div class="flex flex-wrap rounded-3xl overflow-hidden bg-d_darkGreen">
+		{#each slice.primary.items as item, index}
+			<div class="w-1/3 md:w-1/6 aspect-square relative">
+				<div class="hover:opacity-0 transition-opacity duration-200 absolute z-20 w-full h-full aspect-square top-0 left-0 
+					{index % 2 === 0 ? 'bg-d_lightGreen' : 'bg-d_darkGreen'}">
 					<PrismicImage class="w-full h-full p-4 md:p-8 lg:p-10 object-contain" field={item.logo} />
 				</div>
 				
